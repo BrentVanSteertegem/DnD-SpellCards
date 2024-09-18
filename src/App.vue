90d777f844c9e0f1bae4e800241cf457e81612e8
Brent Van Steertegem <brent.vansteertegem@gmail.com>
2024-09-18 14:42:06 +0200

    feat: Get all spells from api + save spells to localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
-  <SpellCard name="Hello World"/>
+  <li v-for="spell in spells" :key="spell.id">
+    <SpellCard :name="spell.name"/>
+  </li>
 </template>
 
 <script>
   import { SpellCard } from './components/index.ts'
+  import { getAllSpells } from './data/getAllSpells.ts'
 
   export default {
     name: 'App',
     components: {
-      SpellCard
+      SpellCard,
+    },
+    data() {
+      return {
+        spells: []
+      }
+    },
+    mounted() {
+      const fetchSpells = async () => {
+        const savedSpells = localStorage.getItem('spells')
+        if (savedSpells) {
+          this.spells = JSON.parse(savedSpells)
+        }
+
+        const apiSpells = await getAllSpells()
+        if (apiSpells) {
+          this.spells = apiSpells
+          localStorage.setItem('spells', JSON.stringify(apiSpells))
+        }
+      }
+
+      fetchSpells()
     }
   }
 </script>
