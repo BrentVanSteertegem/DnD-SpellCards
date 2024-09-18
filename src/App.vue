0ec13d444e9f16febfe3763ba7d1493359455b23
Brent Van Steertegem <brent.vansteertegem@gmail.com>
2024-09-18 13:24:05 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <SpellCard name="Hello World"/>
+</template>
+
+<script>
+  import { SpellCard } from './components/index.ts'
+
+  export default {
+    name: 'App',
+    components: {
+      SpellCard
+    }
+  }
+</script>
---
+++ src/components/SpellCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>{{ name }}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'SpellCard',
+    props: {
+      name: String
+    }
+  }
+</script>
